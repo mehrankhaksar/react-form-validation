3e4ee7d576222e9f4d0e72de4854c7a750053167
Mehran Khaksar <itsaboutmehran@yahoo.com>
2021-10-01 23:21:49 +0330

    media queries almost completed

--- src/components/signupform/SignUpForm.js
+++ src/components/signupform/SignUpForm.js
@@ -16,7 +16,7 @@ const SignUpForm = () => {
                     <label className={!(focus.username) ? "form__label" : "form__label focus"}>Username</label>
                     <input type="text" name="username" value={values.username} className="form__input" onChange={changeHandler} onFocus={addFocus} onBlur={removeFocus} />
                     {(errors.username && focus.username) && <i class='bx bx-error-circle form__icon-error'></i>}
-                    {(errors.username && focus.username) && <span className="form__error"><i class='bx bxs-left-arrow' id="icon__arrow"></i>{errors.username}</span>}
+                    {(errors.username && focus.username) && <span className="form__error"><i class='bx bxs-left-arrow' id="arrow__icon"></i>{errors.username}</span>}
                 </div>
             </div>
             <div className={!(focus.email) ? "form__field" : "form__field focus"}>
@@ -26,7 +26,7 @@ const SignUpForm = () => {
                     <label className={!(focus.email) ? "form__label" : "form__label focus"}>Email</label>
                     <input type="text" name="email" value={values.email} className="form__input" onChange={changeHandler} onFocus={addFocus} onBlur={removeFocus} />
                     {(errors.email && focus.email) && <i class='bx bx-error-circle form__icon-error'></i>}
-                    {(errors.email && focus.email) && <span className="form__error"><i class='bx bxs-left-arrow' id="icon__arrow"></i>{errors.email}</span>}
+                    {(errors.email && focus.email) && <span className="form__error"><i class='bx bxs-left-arrow' id="arrow__icon"></i>{errors.email}</span>}
                 </div>
             </div>
             <div className={!(focus.password) ? "form__field" : "form__field focus"}>
@@ -36,7 +36,7 @@ const SignUpForm = () => {
                     <label className={!(focus.password) ? "form__label" : "form__label focus"}>Password</label>
                     <input type="text" name="password" value={values.password} className="form__input" onChange={changeHandler} onFocus={addFocus} onBlur={removeFocus} />
                     {(errors.password && focus.password) && <i class='bx bx-error-circle form__icon-error'></i>}
-                    {(errors.password && focus.password) && <span className="form__error"><i class='bx bxs-left-arrow' id="icon__arrow"></i>{errors.password}</span>}
+                    {(errors.password && focus.password) && <span className="form__error"><i class='bx bxs-left-arrow' id="arrow__icon"></i>{errors.password}</span>}
                 </div>
             </div>
             <div className={!(focus.confirmPassword) ? "form__field" : "form__field focus"}>
@@ -46,7 +46,7 @@ const SignUpForm = () => {
                     <label className={!(focus.confirmPassword) ? "form__label" : "form__label focus"}>Confirm Password</label>
                     <input type="text" name="confirmPassword" value={values.confirmPassword} className="form__input" onChange={changeHandler} onFocus={addFocus} onBlur={removeFocus} />
                     {(errors.confirmPassword && focus.confirmPassword) && <i class='bx bx-error-circle form__icon-error'></i>}
-                    {(errors.confirmPassword && focus.confirmPassword) && <span className="form__error"><i class='bx bxs-left-arrow' id="icon__arrow"></i>{errors.confirmPassword}</span>}
+                    {(errors.confirmPassword && focus.confirmPassword) && <span className="form__error"><i class='bx bxs-left-arrow' id="arrow__icon"></i>{errors.confirmPassword}</span>}
                 </div>
             </div>
             <button type="submit" className="form__btn">Sign Up</button>
